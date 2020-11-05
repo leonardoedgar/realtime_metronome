a986b97df681cae7fa03800184474a5cabcec97c
leonardoedgar <leonardo_edgar98@outlook.com>
2020-11-05 16:28:45 +0800

    implement reading a param from a config file

---
+++ examples/read_config_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int get_config(char* config) {
+    FILE *stream;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    bool keyFound;
+    int configData = 0;
+    stream = fopen("config.txt", "r");
+    if (stream == NULL) {
+        printf("FileNotFound\n");
+    }
+    else {
+        while ((read = getline(&line, &len, stream)) != -1) {
+            if (strstr(line, config)) {
+                line = strstr(line, "=");
+                keyFound = true;
+                break;
+            }
+        }
+        if (keyFound) {
+            configData = atoi(&line[1]);
+        }
+        fclose(stream);
+    }
+    return configData;
+}
+
+int main() {
+    int bpm = get_config("BPM");
+    printf("BPM = %d\n", bpm);
+    int apple = get_config("APPLE");
+    printf("APPLE = %d\n", apple);
+    return 0;
+}
